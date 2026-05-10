c4374bb50087ed73e3b52b147b3cbec8dd91e788
Samuel Martos <samuelmv04@gmail.com>
2026-05-10 15:24:10 +0200

    spring boot

---
+++ calculatorMVS/src/main/java/com/dam/calculatorMVS/CalculatorMvsApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorMVS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorMvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorMvsApplication.class, args);
+	}
+
+}
