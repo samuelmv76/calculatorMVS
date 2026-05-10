eff6a90d578a4bfbef02bb97c32c7f8982eb3857
Samuel Martos <samuelmv04@gmail.com>
2026-05-10 16:25:03 +0200

    fin?

--- calculatorMVS/src/test/java/com/dam/calculatorMVS/CalculatorMvsApplicationTests.java
+++ calculatorMVS/src/test/java/com/dam/calculatorMVS/CalculatorMvsApplicationTests.java
@@ -1,13 +1,24 @@
 package com.dam.calculatorMVS;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CalculatorMvsApplicationTests {
 
+	@Autowired
+	private Calculator calculator;
+
 	@Test
 	void contextLoads() {
 	}
+/* 
+	@Test
+	public void testSumIncorrecto() {
+		assertEquals(7, calculator.sum(2, 3));
+	}
+*/
 
 }
